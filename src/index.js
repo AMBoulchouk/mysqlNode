ebc924f561367d8ebf490634f4a03bbcb7aab09d
Alex M Boulchouk <amboulchouk@outlook.com>
2022-10-12 17:02:03 -0300

    handlebars cambió bastante, hay que actualizar

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const app = express();
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('.hbs', exphbs.engine({
+    defaultView: 'main',
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
